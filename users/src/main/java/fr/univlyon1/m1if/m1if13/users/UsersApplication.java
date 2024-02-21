4c2ef518dea17e714a5a93c756eb81966b8335e8
HASSAN BABIKER Wail <wailhassanbabiker@gmail.com>
2024-02-21 16:53:03 +0100

    ignore

---
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/UsersApplication.java
@@ -0,0 +1,13 @@
+package fr.univlyon1.m1if.m1if13.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApplication.class, args);
+	}
+
+}
