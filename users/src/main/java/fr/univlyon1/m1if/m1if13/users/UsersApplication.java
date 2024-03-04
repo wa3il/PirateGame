47784d845b9794b44378d75e688464e8233013ff
Erkan <eaksoy2211@gmail.com>
2024-03-04 16:17:20 +0100

    Implémentation UsersApplication

--- users/src/main/java/fr/univlyon1/m1if/m1if13/users/UsersApplication.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/UsersApplication.java
@@ -3,11 +3,31 @@ package fr.univlyon1.m1if.m1if13.users;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Optional;
+
 @SpringBootApplication
 public class UsersApplication {
 
+	private static UserDao userDao;
+
 	public static void main(String[] args) {
-		SpringApplication.run(UsersApplication.class, args);
+		userDao = new UserDao();
+
+		User user1 = getUser("John");
+		System.out.println(user1);
+		userDao.update(user1, new String[]{"Jake", "PIRATE", "password123"});
+
+		User user2 = getUser("Susan");
+		userDao.delete(user2);
+		userDao.save(new User("Julie", Species.VILLAGEOIS,"password567"));
+
+		userDao.getAll().forEach(user -> System.out.println(user.getLogin() + " - " + user.getSpecies()));
 	}
 
+	private static User getUser(String login) {
+		Optional<User> user = userDao.get(login);
+
+		return user.orElseGet(
+				() -> new User("non-existing user", Species.VILLAGEOIS, "no-email"));
+	}
 }
