7b06ae190553c38b5619688fdbabe42ff539d8c5
Erkan <eaksoy2211@gmail.com>
2024-04-03 17:08:47 +0200

    Implémentation Serveur de fichiers statiques

--- api/index.js
+++ api/index.js
@@ -1,11 +1,27 @@
 import express from 'express';
+import path from 'path';
+
 const app = express();
 const port = 3000;
 
+// Obtenir le chemin absolu du répertoire 'public'
+const publicPath = path.resolve('public');
+
+// Middleware pour servir des fichiers statiques depuis le dossier public
+app.use(express.static(publicPath));
+
+// Redirection de la requête à la racine vers index.html
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+// Middleware pour gérer les erreurs 404
+app.use((req, res, next) => {
+	res.status(404).send("Sorry, can't find that!");
+	next();
+});
+
+// Démarrer le serveur
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`);
 });
