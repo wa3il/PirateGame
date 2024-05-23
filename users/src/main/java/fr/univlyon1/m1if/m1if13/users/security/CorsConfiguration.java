3ad4cec081d550c56d01c7ad6c417d13647a332e
wail <wailhassanbabiker@gmail.com>
2024-05-23 22:34:34 +0200

    main

--- users/src/main/java/fr/univlyon1/m1if/m1if13/users/security/CorsConfiguration.java
+++ users/src/main/java/fr/univlyon1/m1if/m1if13/users/security/CorsConfiguration.java
@@ -15,6 +15,7 @@ public class CorsConfiguration  {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost/", "http://192.168.75.124/", "https://192.168.75.124")
+                        .allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE");
             }
         };
