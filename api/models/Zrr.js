36c75d6aa24304cf2d52957e522f708954c7ca2e
wail <wailhassanbabiker@gmail.com>
2024-05-26 16:21:40 +0200

    mdf tp4

--- api/models/Zrr.js
+++ api/models/Zrr.js
@@ -7,10 +7,10 @@
 // Path: api/models/Zrr.js
 
 class Zrr {
-    constructor(point1, point2) {
-        this.point1 = point1;
-        this.point2 = point2;
-    }
+	constructor(point1, point2) {
+		this.point1 = point1;
+		this.point2 = point2;
+	}
 }
 
 export default Zrr;
--- api/public/static/js/index.js
+++
@@ -1,6 +0,0 @@
-import './css/style.css';
-import map from './map.js';
-import { initListeners } from './form.js';
-
-const mymap = map();
-initListeners(mymap);
